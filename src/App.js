eb78823fc4ed6b690d0c2ba7a497fc8692f63222
Park Minsoo <pms2259@naver.com>
2024-01-17 20:38:58 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import './App.css';
+import Template from './components/Template';
+import TodoList from './components/TodoList';
+
+const App = () => {
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "할일 1",
+      checked: true
+    },
+    {
+      id: 2,
+      text: "할일 2",
+      checked: false
+    },
+    {
+      id: 3,
+      text: "할일 3",
+      checked: true
+    }
+  ])
+
+  return (
+    <Template todoLength={todos.length}>
+      <TodoList todos={todos}/>
+    </Template>
+  )
+}
+
+export default App;
