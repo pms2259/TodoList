bad4c9fa8e608fb1776ced336bd1854f9e18de0e
Park Minsoo <pms2259@naver.com>
2024-01-18 23:15:31 +0900

    TodoInsert error

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, {useState} from 'react';
 import './App.css';
 import Template from './components/Template';
 import TodoList from './components/TodoList';
+import TodoInsert from './components/TodoInsert';
 import { MdAddCircle  } from "react-icons/md";
+
+let nextId = 4;
 
 const App = () => {
 
+  const [insertToggle, setInsertToggle] = useState(false);
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -24,12 +28,29 @@ const App = () => {
     }
   ])
 
+  const onInsertToggle = () => {
+    setInsertToggle(prev => !prev);
+  }
+
+  const onIsertTodo = (text) => {
+    if (text === "") {
+      return alert('할 일을 입력해주세요.')
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        checked: false
+      }
+      setTodos(todos => todos.concat(todo));
+    }
+  }
   return (
     <Template todoLength={todos.length}>
       <TodoList todos={todos} />
-      <div className='add-todo-button'>
+      <div className='add-todo-button' onClick={onInsertToggle}>
         <MdAddCircle />
       </div>
+      {insertToggle && <TodoInsert onInsertToggle={onInsertToggle} />}
     </Template>
   )
 }
